3c79c8fb1aa1919d57f95bda02f4fe0053b05c13
DarkEXE <aconnor0213+cryptic@gmail.com>
2024-11-23 22:19:14 +0000

    Current files before logging off, gonna turn this into a improvements mod

--- LunaMod/LunaModPlugin.cs
+++ LunaMod/LunaModPlugin.cs
@@ -1,34 +1,72 @@
-﻿using BepInEx;
-using BepInEx.Configuration;
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BepInEx;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using LunaMod.Utilities;
 using Reactor;
 using Reactor.Utilities;
 
 namespace LunaMod;
 
-[BepInAutoPlugin]
+[BepInPlugin(Id, "LunaMod", modVersion)]
 [BepInProcess("Among Us.exe")]
 [BepInDependency(ReactorPlugin.Id)]
 public partial class LunaModPlugin : BasePlugin
 {
-    public Harmony Harmony { get; } = new(Id);
+    public const string Id = "me.anyastudios.lunamod";
+    public const string modVersion = "1.0";
+    public static bool isDev = false;
 
-    public ConfigEntry<string> ConfigName { get; private set; }
+    public static System.Version Version = System.Version.Parse(modVersion);
+    public Harmony Harmony { get; } = new(Id);
 
     public override void Load()
     {
-        ConfigName = Config.Bind("Fake", "Name", ":>");
+        LunaLogger.Message("LunaMod is now loading!");
 
-        Harmony.PatchAll();
+        bool dev = CheckDevRelease().Result;
+        isDev = dev;
+
+        ReactorCredits.Register("LunaMod", modVersion + (dev ? "-indev" : ""), false, ReactorCredits.AlwaysShow);
     }
 
-    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
-    public static class ExamplePatch
+    public static async Task<bool> CheckDevRelease()
     {
-        public static void Postfix(PlayerControl __instance)
+        string url = $"https://github.com/AnyaStudios/AmongUs-LunaMod/tags";
+        bool[] exists = await HttpVersionExists(url);
+        LunaLogger.Info($"IsDevRelease: {!exists[0]} ({(exists[1] ? "success" : "fail")})");
+        return !exists[0];
+    }
+
+    static async Task<bool[]> HttpVersionExists(string url)
+    {
+        bool found = false;
+        using HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "C# App");
+        try
         {
-            __instance.cosmetics.nameText.text = PluginSingleton<LunaModPlugin>.Instance.ConfigName.Value;
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
+            using JsonDocument jsonDoc = JsonDocument.Parse(content);
+            var root = jsonDoc.RootElement;
+            foreach (var tag in root.EnumerateArray())
+            {
+                string name = tag.GetProperty("name").GetString();
+                if (name == modVersion)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            return [found, true];
+        }
+        catch (HttpRequestException ex)
+        {
+            LunaLogger.Error($"Failed To Check For Dev Release: {ex.Message}");
+            return [false, false];
         }
     }
 }
---
+++ LunaMod/Utilities/LunaLogger.cs
@@ -0,0 +1,36 @@
+﻿using Reactor.Utilities;
+
+namespace LunaMod.Utilities;
+
+public static class LunaLogger
+{
+    public static void Debug(string message)
+    {
+        Logger<LunaModPlugin>.Debug(message);
+    }
+
+    public static void Info(string message)
+    {
+        Logger<LunaModPlugin>.Info(message);
+    }
+
+    public static void Message(string message)
+    {
+        Logger<LunaModPlugin>.Message(message);
+    }
+
+    public static void Warn(string message)
+    {
+        Logger<LunaModPlugin>.Warning(message);
+    }
+
+    public static void Error(string message)
+    {
+        Logger<LunaModPlugin>.Debug(message);
+    }
+
+    public static void Fatal(string message)
+    {
+        Logger<LunaModPlugin>.Fatal(message);
+    }
+}
